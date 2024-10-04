2e447418ca6ba39bcb649063959fe937afc773f9
GOMES31 <dugomes02@gmail.com>
2024-10-04 19:35:22 +0300

    nav bar added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navbarMenu = document.querySelector('.navbar-menu');
-const toggleIcon = mobileMenu.querySelector('img');
+const navEl = document.querySelector(".nav");
+const hamburgerEl = document.querySelector(".hamburger");
 
-mobileMenu.addEventListener('click', () => {
-    navbarMenu.classList.toggle('active');
-    if (navbarMenu.classList.contains('active')) {
-        toggleIcon.src = 'icons/close.svg';
-        toggleIcon.alt = 'Close Menu';
-    } else {
-        toggleIcon.src = 'icons/hamburger.svg';
-        toggleIcon.alt = 'Open Menu';
-    }
+hamburgerEl.addEventListener("click", () => {
+    navEl.classList.toggle("nav-open");
+    hamburgerEl.classList.toggle("hamburger-open");
+});
+
+navEl.addEventListener("click", () => {
+    navEl.classList.remove("nav-open");
+    hamburgerEl.classList.remove("hamburger-open");
 });
