34f8122639a3a9d206e5fab6dff5c99682bb7e26
GOMES31 <dugomes02@gmail.com>
2024-10-08 13:24:51 +0300

    Removed navbar, added terminal about me

--- js/terminal.js
+++ js/terminal.js
@@ -1,18 +1,25 @@
 const terminalLines = [
-    '<span class="name">Eduardo.currentLocation</span>',
-    '"Braga, Portugal"',
+    '<span class="name">Name</span>',
+    '[Eduardo Gomes]',
     '',
-    '<span class="name">Eduardo.contactInfo</span>',
-    '["dugomes02@gmail.com", "LinkedIn", "GitHub"]',
+    '<span class="name">Location</span>',
+    '[Braga, Portugal]',
     '',
-    '<span class="name">Eduardo.resume</span>',
-    '"resume.pdf"',
+    '<span class="name">Contact</span>',
+    '[<a href="mailto:dugomes02@gmail.com">dugomes02@gmail.com</a>, <a href="https://www.linkedin.com/in/eduardogomes31/" target="_blank">LinkedIn</a>, <a href="https://github.com/GOMES31" target="_blank">GitHub</a>]',
     '',
-    '<span class="name">Eduardo.skills</span>',
-    '["HTML", "CSS", "Java", "SpringBoot", "Thymeleaf"]'
+    '<span class="name">Resume</span>',
+    '[<a href="resume.pdf" target="_blank">resume.pdf</a>]',
+    '',
+    '<span class="name">Skills</span>',
+    '[HTML, CSS, Java, SpringBoot, Thymeleaf]'
 ];
 
 let currentLine = 0;
+let currentChar = 0;
+let currentText = '';
+let isTag = false;
+let tagBuffer = '';
 
 const terminalOutput = document.getElementById("terminal-output");
 
@@ -20,20 +27,38 @@ function typeLine() {
     if (currentLine < terminalLines.length) {
         let line = terminalLines[currentLine];
 
-        if (line.length > 0) {
-            let paragraph = document.createElement('p');
-            paragraph.innerHTML = line;
-            terminalOutput.appendChild(paragraph);
-            currentLine++;
-            setTimeout(typeLine, 250);
+        if (currentChar < line.length) {
+            let char = line[currentChar];
+
+            if (char === '<') {
+                isTag = true;
+                tagBuffer += char;
+            } else if (char === '>') {
+                isTag = false;
+                tagBuffer += char;
+                currentText += tagBuffer;
+                terminalOutput.lastChild.innerHTML = currentText;
+                tagBuffer = '';
+            } else if (isTag) {
+                tagBuffer += char;
+            } else {
+                currentText += char;
+                terminalOutput.lastChild.innerHTML = currentText;
+            }
+
+            currentChar++;
+            setTimeout(typeLine, 20);
         } else {
-            terminalOutput.innerHTML += "<p></p>";
+            terminalOutput.appendChild(document.createElement('p'));
             currentLine++;
-            setTimeout(typeLine, 250);
+            currentChar = 0;
+            currentText = '';
+            setTimeout(typeLine, 150);
         }
     }
 }
 
 window.onload = () => {
+    terminalOutput.appendChild(document.createElement('p'));
     setTimeout(typeLine, 100);
 };
